a671903562d0a01c78eab0244276fbd8e926edd8
Christian Bringino <christiandevbringino@gmail.com>
2024-07-31 14:21:14 +0800

    [EmployeeApp] Add employee params

---
+++ iOS/Swift/EmployeeApp/EmployeeApp/Modules/Employees/Models/Requests/EmployeeParams.swift
@@ -0,0 +1,26 @@
+//
+//  EmployeeParams.swift
+//  EmployeeApp
+//
+//  Created by Christian Bringino on 7/31/24.
+//
+
+import Foundation
+
+struct EmployeeParams {
+    var firstname: String?
+    var middlename: String?
+    var lastname: String?
+    var gender: Gender?
+    var birthdate: Date?
+    var email: String?
+    var phoneNumber: String?
+    var role: String?
+    var startDate: Date?
+    var endDate: String?
+    var reasonForLeaving: String?
+    var technicalSkills: [String]?
+    var personalSkills: [String]?
+    var resumeURL: String?
+    var imageURL: String?
+}
